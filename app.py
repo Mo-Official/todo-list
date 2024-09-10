57683c414717b36c63d02520bdcfa952f88e6a01
Mo-Official <mouaztabboush@gmail.com>
2024-09-10 13:59:00 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template, redirect, request
+
+app = Flask(__name__, template_folder="./templates")
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run("", debug=True)
